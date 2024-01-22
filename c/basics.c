23e163369eda4c4549875dbfe53461b9e54e776c
CorinStaples <2027staples.corin@fcboe.org>
2024-01-22 15:41:08 -0500

    message

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int num;
+	
+	printf("This is my c basics program\n");
+	printf("Enter a number: ");
+	scanf("%i", &num);
+	printf("You entered %i\n", num);
+	return 0;
+	
+}
