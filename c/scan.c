654838a6ea5806049db3d98c7b54b80573464573
CorinStaples <2027staples.corin@fcboe.org>
2024-01-24 15:14:06 -0500

    Age, GPA, and Letter grade added

---
+++ c/scan.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int age;
+	float gpa;
+	char grade;
+
+	printf("How old are you? ");
+	scanf("%i", &age);
+	printf("What is your gpa? ");
+	scanf("%f", &gpa);
+	printf("What is your current letter grade? ");
+	scanf(" %c", &grade);
+	printf("Age: %i\n", age);
+	printf("GPA: %f\n", gpa);
+	printf("Letter Grade: %c\n", grade);
+
+	return 0;
+}
